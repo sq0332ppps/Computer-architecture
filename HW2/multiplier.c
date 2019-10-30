479c163490dc01d1929bc1555e4da67f74cd7aba
sq0332ppps <sq0332ppps>
2019-10-30 14:34:09 +0800

    upl

--- HW2/multiplier.c
+++ HW2/multiplier.c
@@ -3,19 +3,15 @@ void _start()
 {
     int mul1 = -1;
     int mul2 = -2;
-    int* result = (int*) 0x15000;
+    int result = 0;
 
     for(int i=0;i<32;i++)
     {
         if((mul2 >> i) & 0x1)
-            *result = *result + (mul1 << i);              
+            result = result + (mul1 << i);              
     }
 
     volatile  int* tx = (volatile int*) 0x40002000;
-   
-    
-    *tx = *result;
-    
-
+    *tx = result;
 }
 
